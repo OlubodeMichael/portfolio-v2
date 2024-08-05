d5bc500bc6ce1ebf10767a48dee75e19a649f882
OlubodeMichael <moolubode3@gmail.com>
2024-08-05 13:34:52 -0400

    About: I added icons

--- app/_components/Technologies.js
+++ app/_components/Technologies.js
@@ -1,33 +1,49 @@
 "use client"
 import React from 'react';
-import Ticker, { TICKER_DIRECTION_LEFT, TICKER_DIRECTION_RIGHT } from 'framer-motion-ticker';
-import { IoLogoJavascript } from "react-icons/io";
+import Ticker from 'framer-motion-ticker';
+import { TICKER_DIRECTION_LEFT, TICKER_DIRECTION_RIGHT } from 'framer-motion-ticker';
+import { 
+    CPlusPlusIcon, JavaScriptIcon, PythonIcon, 
+    ReactIcon, SwiftIcon, FirebaseIcon, 
+    JavaIcon, LinuxIcon 
+} from "developer-icons"; 
 
+export default function Technologies({ left }) {
+  const icons = [
+    { component: <CPlusPlusIcon />, label: 'C++' },
+    { component: <JavaScriptIcon />, label: 'JavaScript' },
+    { component: <PythonIcon />, label: 'Python' },
+    { component: <ReactIcon />, label: 'React' },
+    { component: <SwiftIcon />, label: 'Swift' },
+    { component: <FirebaseIcon />, label: 'Firebase' },
+    { component: <JavaIcon />, label: 'Java' },
+    { component: <LinuxIcon />, label: 'Linux' },
+  ];
+  let direction = left ? TICKER_DIRECTION_LEFT : TICKER_DIRECTION_RIGHT;
 
-export default function Technologies({left}) {
-    const colors = ['#632bf3', '#f122c8', '#f16022', '#9ef344', '#44d3f3'];
-    let direction;
-    if (left) {
-      direction = TICKER_DIRECTION_LEFT;
-    } else {
-      direction = TICKER_DIRECTION_RIGHT;
-    }
-    return (
-        <div>
-            <Ticker duration={20} direction={direction} >
-        {colors.map((item, index) => (
+  return (
+    <div className="overflow-hidden">
+      <DisplayList list={icons} direction={direction}/>
+    </div>
+  );
+}
+
+function DisplayList({ list, direction }) {
+  return (
+    <div className="overflow-hidden">
+      <Ticker duration={20} direction={direction}>
+        {list.map((item) => (
           <div
-            key={index}
-            style={{
-              backgroundColor: item,
-              margin: '5px',
-              height: '250px',
-              width: '200px',
-            }}
-          />
+            key={item.label}
+            className="flex flex-col items-center justify-center mx-4 h-40 w-40"
+          >
+            <div className="flex items-center justify-center h-12 w-12">
+              {item.component}
+            </div>
+            <span className="mt-1 text-white text-sm">{item.label}</span>
+          </div>
         ))}
       </Ticker>
-      
-        </div>
-    );
+    </div>
+  )
 }
