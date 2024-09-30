cae560c766ca702c3c310f464f10ac06e6b2fede
OlubodeMichael <moolubode3@gmail.com>
2024-09-30 11:28:38 -0400

    Added dev icons

--- app/_components/Technologies.js
+++ app/_components/Technologies.js
@@ -4,26 +4,16 @@ import Ticker from 'framer-motion-ticker';
 import { TICKER_DIRECTION_LEFT, TICKER_DIRECTION_RIGHT } from 'framer-motion-ticker';
 import { 
     CPlusPlusIcon, JavaScriptIcon, PythonIcon, 
-    ReactIcon, SwiftIcon, FirebaseIcon, 
-    JavaIcon, LinuxIcon 
+    ReactIcon, SwiftIcon, MySQLIcon, 
+    JavaIcon, HTML5Icon, NodeJsIcon, NextJsIcon, ExpressJsLightIcon, TailwindCSSIcon
 } from "developer-icons"; 
 
-export default function Technologies({ left }) {
-  const icons = [
-    { component: <CPlusPlusIcon />, label: 'C++' },
-    { component: <JavaScriptIcon />, label: 'JavaScript' },
-    { component: <PythonIcon />, label: 'Python' },
-    { component: <ReactIcon />, label: 'React' },
-    { component: <SwiftIcon />, label: 'Swift' },
-    { component: <FirebaseIcon />, label: 'Firebase' },
-    { component: <JavaIcon />, label: 'Java' },
-    { component: <LinuxIcon />, label: 'Linux' },
-  ];
+export default function Technologies({ left, list }) {
   let direction = left ? TICKER_DIRECTION_LEFT : TICKER_DIRECTION_RIGHT;
 
   return (
     <div className="overflow-hidden">
-      <DisplayList list={icons} direction={direction}/>
+      <DisplayList list={list} direction={direction}/>
     </div>
   );
 }
@@ -37,7 +27,7 @@ function DisplayList({ list, direction }) {
             key={item.label}
             className="flex flex-col items-center justify-center mx-4 h-40 w-40"
           >
-            <div className="flex items-center justify-center h-12 w-12">
+            <div className="flex items-center justify-center">
               {item.component}
             </div>
             <span className="mt-1 text-white text-sm">{item.label}</span>
--- app/about/page.js
+++ app/about/page.js
@@ -3,6 +3,11 @@ import SubTitle from "../_components/SubTitle";
 import Text from "../_components/Text";
 import ExperienceCard from "../_components/ExperienceCard";
 import Technologies from "@/app/_components/Technologies";
+import { 
+    CPlusPlusIcon, JavaScriptIcon, PythonIcon, 
+    ReactIcon, SwiftIcon, MySQLIcon, 
+    JavaIcon, HTML5Icon, NodeJsIcon, NextJsIcon, ExpressJsDarkIcon, TailwindCSSIcon, ReactNativeIcon,
+} from "developer-icons"; 
 
 
 export const metadata = {
@@ -10,6 +15,25 @@ export const metadata = {
 }
 
 export default function About() {
+    const languages = [
+        { component: <CPlusPlusIcon />, label: 'C++' },
+        { component: <JavaScriptIcon />, label: 'JavaScript' },
+        { component: <PythonIcon />, label: 'Python' },
+        { component: <SwiftIcon />, label: 'Swift' },
+        { component: <JavaIcon />, label: 'Java' },
+        { component: <HTML5Icon />, label: 'HTML' },
+        { component: <MySQLIcon />, label: 'Sql' },
+        //{ component: <ReactNativeIcon />, label: 'React Native'}
+        
+      ];
+    
+      const frameworks = [
+        { component: <ReactIcon />, label: 'ReactJS' },
+        { component: <NodeJsIcon />, label: 'NodeJs' },
+        { component: <NextJsIcon />, label: 'NextJS' },
+        { component: <ExpressJsDarkIcon />, label: 'ExpressJs' },
+        { component: <TailwindCSSIcon />, label: 'Tailwind css' },
+      ]
     return (
     <div>
         <Title>About</Title>
@@ -25,19 +49,12 @@ export default function About() {
         {/*Skills section */}
         <SubTitle>Skills</SubTitle>
         <h6>Programming Languages</h6>
-        <Technologies left={true}/>
-        <Technologies left={false}/>
+        <Technologies left={true} list={languages}/>
+        <h6 className="">Frameworks</h6>
+        <Technologies left={false} list={frameworks}/>
 
         {/*Experience section */}
         <SubTitle>Experiences</SubTitle>
-        <ExperienceCard 
-            date="Jan - May 2024"
-            title="IOS Developer"
-            technologies={["Swift", "Xcode", "Firebase", "Firestore", "Final Cut", "Lucid Charts"]}
-            role="Backend engineer / Manager"
-        >
-            Spearheaded the full-stack development of an iOS mental health platform funded by an Apple innovation grant. Leveraged Swift, Xcode, and Firebase for robust authentication and Firestore for real-time encrypted messaging. Engineered and optimized the application's architecture for high performance and scalability, employing unit and integration testing frameworks to ensure fault tolerance and seamless user interactions. Authored detailed technical documentation and created README videos and app flow diagrams using Final Cut and Lucid Charts, enhancing developer handoff and user understanding.
-        </ExperienceCard>
 
         <ExperienceCard
             date="Jun 2024 - Present"
@@ -47,7 +64,14 @@ export default function About() {
         >
             Maintained and enhanced an existing Python framework for compiling pre-written JavaScript components, facilitating data-driven web applications. Contributed to feature planning and task delegation while supporting the Continuous Deployment pipeline in Jenkins for Python Sphinx documentation. Played a key role in developing, testing, and integrating new functionalities for a React web application using agile methodologies. Monitored satellite data product flow, provided prompt customer support during outages, and utilized AWS S3 and DynamoDB to extend the framework’s capabilities. Engaged in collaborative efforts to ensure seamless project execution and improve overall system performance.
         </ExperienceCard>
-        
+        <ExperienceCard 
+            date="Jan - May 2024"
+            title="IOS Developer"
+            technologies={["Swift", "Xcode", "Firebase", "Firestore", "Final Cut", "Lucid Charts"]}
+            role="Backend engineer / Manager"
+        >
+            Spearheaded the full-stack development of an iOS mental health platform funded by an Apple innovation grant. Leveraged Swift, Xcode, and Firebase for robust authentication and Firestore for real-time encrypted messaging. Engineered and optimized the application's architecture for high performance and scalability, employing unit and integration testing frameworks to ensure fault tolerance and seamless user interactions. Authored detailed technical documentation and created README videos and app flow diagrams using Final Cut and Lucid Charts, enhancing developer handoff and user understanding.
+        </ExperienceCard>
     </div>
     );
   }
