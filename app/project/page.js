ea96be8ae4114e42886d492d8fc8072d10a31c43
OlubodeMichael <moolubode3@gmail.com>
2025-02-17 10:42:52 -0500

    add c++ http server project

--- app/project/page.js
+++ app/project/page.js
@@ -11,6 +11,15 @@ export default function Project() {
     <div>
         <Title>My Projects</Title>
         <ProjectCard 
+            title="High-Performance Multithreaded HTTP Server in C++" 
+            descriptions="Developed a high-performance, multithreaded HTTP server in C++ to efficiently handle client requests and serve static files. The server supports concurrent connections, allowing multiple users to access resources simultaneously. Implemented custom request processing to serve HTML, CSS, JavaScript, images, and API responses in JSON format.
+            Designed with socket programming and multithreading, the server optimizes request handling for better performance. Integrated logging mechanisms to track server events and client interactions, ensuring robust monitoring. Added caching strategies to improve response times and reduce redundant processing.
+            Future enhancements include SSL/TLS encryption for secure HTTPS connections, WebSocket support for real-time applications, and containerization using Docker for scalable deployments."
+            technologies={["C++", "Multithreading", "Socket Programming", "HTTP Protocol", "File I/O", "JSON Parsing"]}
+            link="https://github.com/OlubodeMichael/HTTPServer"
+          />
+
+        <ProjectCard 
           title="Search Engine" 
           descriptions="Developed a full-stack search engine application utilizing Python for backend processing and React.js for the frontend interface. Implemented web crawling and indexing algorithms to efficiently gather and organize web content. Engineered a responsive search interface with real-time results display and advanced filtering capabilities. Utilized Python's natural language processing libraries for text analysis and ranking algorithms to deliver relevant search results. Optimized query performance and implemented caching mechanisms to handle concurrent user searches effectively."
           technologies={["Python", "React.js", "HTML", "CSS", "RESTful API", "Natural Language Processing"]}
@@ -23,6 +32,7 @@ export default function Project() {
           technologies={["React Native", "JavaScript", "Firebase", "CSS"]}
           link="https://github.com/OlubodeMichael/PatiCam"
         />
+
       </div>
     );
   }
